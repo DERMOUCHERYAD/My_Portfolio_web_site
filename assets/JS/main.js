4ee6b309accf5b806dd5e42921cc2a59017287ff
DERMOUCHERYAD <dermoucheryad@gmail>
2025-01-26 13:15:00 +0100

    Modification1 Js

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -57,7 +57,7 @@ async function notifyVisit() {
     visitData.append('ip_address', visitorIP || 'IP non disponible');
     visitData.append('timestamp', new Date().toLocaleString());
 
-    fetch("https://formspree.io/f/mrbeekeg", {
+    fetch("https://formspree.io/f/xblddyol", {
         method: 'POST',
         headers: {
             'Accept': 'application/json'
